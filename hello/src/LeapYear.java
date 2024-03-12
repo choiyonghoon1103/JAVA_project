d1c596858ce31affc7bb97805c1db905aed3b3bd
hoon <hoon@localhost>
2024-03-12 10:59:39 +0900

    240312

--- hello/src/LeapYear.java
+++ hello/src/LeapYear.java
@@ -1,14 +1,15 @@
-import java.util.*;
 import java.util.Scanner;
 
 public class LeapYear {
   public static void main(String[] args) {
 	  int year;
-      GregorianCalendar gregori = new GregorianCalendar();
       Scanner input = new Scanner(System.in);
       System.out.print("몇년인지 입력하시오 : ");
       year = input.nextInt();
-      if (gregori.isLeapYear(year))
+      boolean dis = true;
+      if (year % 2 != 0)
+    	  dis = false;
+      if (dis)
         System.out.println("윤년입니다.");
       else
         System.out.println("평년입니다.");
